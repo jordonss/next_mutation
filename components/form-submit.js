fc5830ac7c99faf682debc7c53999269488aa5fd
Artem <nemezg@gmail.com>
2025-08-13 16:50:27 +0300

    Add post creation functionality and form submission component

---
+++ components/form-submit.js
@@ -0,0 +1,21 @@
+'use client'
+
+import { useFormStatus } from "react-dom";
+
+export default function FormSubmit({ children }) {
+  const status = useFormStatus();
+
+	if(status.pending) {
+		return (
+			<p>Creating post...</p>
+		)
+	}
+
+
+	return (
+		<>
+			<button type="reset">Reset</button>
+			<button>Create Post</button>
+		</>
+	)
+}
